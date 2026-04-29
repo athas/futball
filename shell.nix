e65ab30b5a757c32ac1640a31e3a2b53a0d05ec7
Troels Henriksen <athas@sigkill.dk>
2026-04-29 09:17:31 +0200

    Bump numbers.

--- shell.nix
+++ shell.nix
@@ -4,10 +4,10 @@ mkShell {
   buildInputs = [
     ocl-icd
     opencl-headers
-    python311
-    python311Packages.pygame
-    python311Packages.numpy
-    python311Packages.pyopencl
+    python313
+    python313Packages.pygame
+    python313Packages.numpy
+    python313Packages.pyopencl
     SDL2
     SDL2_ttf
     futhark
